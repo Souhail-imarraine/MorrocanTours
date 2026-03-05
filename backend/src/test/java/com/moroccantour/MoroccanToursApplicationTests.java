562759502be58e2b19d88c466e0a6eff40afc9f3
imarrainesouhail202@gmail.com <imarrainesouhail202@gmail.com>
2026-03-05 15:52:26 +0000

    Add Test

---
+++ backend/src/test/java/com/moroccantour/MoroccanToursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moroccantour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoroccanToursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
